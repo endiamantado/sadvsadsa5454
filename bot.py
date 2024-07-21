a4f9048a4b422b3973342e6cc0dd2f47a2559e13
endiamantado <dolarbase@gmail.com>
2024-07-20 21:22:24 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,8 @@ DIRECTORY = [
     'cup pami-full-teleconsultado-PantherSearchBot-part1.txt', 
     'gob ar-full-teleconsultado-PantherSearchBot-part1.txt', 
     'gov ar-full-teleconsultado-PantherSearchBot-part1.txt',
-    'a-full-teleconsultado-PantherSearchBot-part1.txt'
+    'a-full-teleconsultado-PantherSearchBot-part1.txt',
+    'banco-full-teleconsultado-PantherSearchBot-part1.txt'
 ]
 
 # Variable global para almacenar los resultados encontrados
